4eae6f5be7359796cc9ca035f414c33fc61f27c1
Alysson Morandi <46841626+alyssonhm@users.noreply.github.com>
2022-01-12 14:31:55 -0300

    Delete extrai-imagem-text directory

--- extrai-imagem-text/extrai-imagem-de-texto.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['pt'])
-
-results = reader.readtext('./extrai-imagem-text/placa.png', paragraph=False, workers=1)
-
-for result in results:
-    print(f'Texto: {result[1]}\n'
-        f'Posição: {result[0]}\n')
